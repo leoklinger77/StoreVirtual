0c87abb680844c29006dfef2f7c35d22ead41c95
Leandro <leandro.klingeroliveira@gmail.com>
2021-02-13 19:27:33 -0300

    NuGet RuntimeCopilation

---
+++ StoreVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Visualizar()
+        {
+            return View();
+        }
+    }
+}
---
+++ StoreVirtual/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace StoreVirtual.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowResquetId => !string.IsNullOrEmpty(RequestId);
+    }
+}
