ada10d9a45c4ac1093f23d1c3ac4b28d9910aa73
Leandro <leandro.klingeroliveira@gmail.com>
2021-02-14 10:34:30 -0300

    Criando a classe de acesso a dados - DbContext

---
+++ StoreVirtual/Data/StoreVirtualContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreVirtual.Data
+{
+    public class StoreVirtualContext : DbContext
+    {
+        public StoreVirtualContext(DbContextOptions<StoreVirtualContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
---
+++ StoreVirtual/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StoreVirtual.Models
+{
+    [Table("Tb_Cliente")]
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+        public DateTime Nascimento { get; set; }        
+        public string Sexo { get; set; }
+        public string CPF { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        
+        
+    }
+}
