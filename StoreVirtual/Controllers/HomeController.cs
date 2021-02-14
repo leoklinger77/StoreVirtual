e9b38789e477c0df76f95a881f2ef044046daa69
Leandro <leandro.klingeroliveira@gmail.com>
2021-02-13 21:12:31 -0300

    Trabalhando com Envio de Email - Gmail

--- StoreVirtual/Controllers/HomeController.cs
+++ StoreVirtual/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StoreVirtual.Models;
+using StoreVirtual.Service.Email;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,7 @@ namespace StoreVirtual.Controllers
 {
     public class HomeController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
@@ -15,6 +18,24 @@ namespace StoreVirtual.Controllers
         public IActionResult Contato()
         {
             return View();
+        }
+        public IActionResult ContatoAcao()
+        {
+            Contato contato = new Contato();
+            contato.Name = HttpContext.Request.Form["nome"];
+            contato.Email = HttpContext.Request.Form["email"];
+            contato.Texto= HttpContext.Request.Form["texto"];
+
+            Service.Email.SendEmail.EnviarContatoEmail(contato);
+
+            return new ContentResult()
+            {
+                Content = string.Format("Dados Recebidos com sucesso! <br/>" +
+                "Nome:{0} <br/>" +
+                "E-mail:{1} <br/>" +
+                "texto:{2}", contato.Name, contato.Email, contato.Texto),
+                ContentType = "text/html"
+            };
         }
         public IActionResult Login()
         {
---
+++ StoreVirtual/Service/Email/SendEmail.cs
@@ -0,0 +1,32 @@
+﻿using StoreVirtual.Models;
+using System.Net;
+using System.Net.Mail;
+
+namespace StoreVirtual.Service.Email
+{
+    public class SendEmail
+    {
+        public static void EnviarContatoEmail(Contato contato)
+        {
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("noreply.leandroklinger@gmail.com", "!@#LEO!@#klinger");
+            smtp.EnableSsl = true;
+
+            string corpMsg = string.Format("<h2>Contato Loja Virtual</h2>" +
+                "<b>Nome: </b> {0} <br/>" +
+                "<b>Email</b> {1} <br/>" +
+                "<b>Texto</b> {2} <br/>" +
+                "<p>E-mail enviado automaticamente do site Loja Virtual</p>",contato.Name,contato.Email,contato.Texto);
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("noreply.leandroklinger@gmail.com");
+            message.To.Add(contato.Email);
+            message.Subject = "Contato - Loja Virtual";
+            message.Body = corpMsg;
+            message.IsBodyHtml = true; //True caso vc queira que o body seja enviado html
+
+            smtp.Send(message);
+        }
+    }
+}
