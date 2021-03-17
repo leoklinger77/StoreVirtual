715ceef9157b9d70ed8b68d99670670bcf4201a7
Leandro <leandro.klingeroliveira@gmail.com>
2021-03-17 15:05:58 -0300

    Repository Cliente

---
+++ StoreVirtual/Repositories/ClienteRepository.cs
@@ -0,0 +1,52 @@
+﻿using StoreVirtual.Data;
+using StoreVirtual.Models;
+using StoreVirtual.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StoreVirtual.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly StoreVirtualContext _context;
+
+        public ClienteRepository(StoreVirtualContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(int id)
+        {
+            Cliente cliente = FindById(id);
+            _context.Remove(cliente);
+            _context.SaveChanges();
+        }
+
+        public ICollection<Cliente> FindAll()
+        {
+            return _context.Cliente.ToList();
+        }
+
+        public Cliente FindById(int id)
+        {
+            return _context.Cliente.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Insert(Cliente cliente)
+        {
+            _context.Cliente.Add(cliente);
+            _context.SaveChanges();
+        }
+
+        public Cliente Login(string email, string password)
+        {
+            return _context.Cliente.Where(x => x.Email == email && x.Senha == password).FirstOrDefault();
+        }
+
+        public void Update(Cliente cliente)
+        {
+            _context.Cliente.Update(cliente);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ StoreVirtual/Repositories/Interfaces/IClienteRepository.cs
@@ -0,0 +1,18 @@
+﻿using StoreVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreVirtual.Repositories.Interfaces
+{
+    public interface IClienteRepository
+    {
+        Cliente Login(string email, string password);
+        Cliente FindById(int id);
+        ICollection<Cliente> FindAll();
+        void Insert(Cliente cliente);
+        void Update(Cliente cliente);
+        void Delete(int id);
+    }
+}
